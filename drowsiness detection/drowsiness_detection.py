7d9e721928bf2260059da341d9cd4da7bb8d9db1
Raflimrv <157944784+Raflimrv@users.noreply.github.com>
2024-08-01 19:51:12 +0700

    Add files via upload

--- drowsiness detection/drowsiness_detection.py
+++ drowsiness detection/drowsiness_detection.py
@@ -1,13 +1,19 @@
 import cv2
 import dlib
 from scipy.spatial import distance
+from playsound import playsound
+import threading
+scan=0
 
 def calculate_EAR(eye):
-	A = distance.euclidean(eye[1], eye[5])
-	B = distance.euclidean(eye[2], eye[4])
-	C = distance.euclidean(eye[0], eye[3])
-	ear_aspect_ratio = (A+B)/(2.0*C)
-	return ear_aspect_ratio
+    A = distance.euclidean(eye[1], eye[5])
+    B = distance.euclidean(eye[2], eye[4])
+    C = distance.euclidean(eye[0], eye[3])
+    ear_aspect_ratio = (A + B) / (2.0 * C)
+    return ear_aspect_ratio
+
+def play_alarm():
+    playsound("alarm.wav")
 
 cap = cv2.VideoCapture(0)
 hog_face_detector = dlib.get_frontal_face_detector()
@@ -19,44 +25,45 @@ while True:
 
     faces = hog_face_detector(gray)
     for face in faces:
-
         face_landmarks = dlib_facelandmark(gray, face)
         leftEye = []
         rightEye = []
 
-        for n in range(36,42):
-        	x = face_landmarks.part(n).x
-        	y = face_landmarks.part(n).y
-        	leftEye.append((x,y))
-        	next_point = n+1
-        	if n == 41:
-        		next_point = 36
-        	x2 = face_landmarks.part(next_point).x
-        	y2 = face_landmarks.part(next_point).y
-        	cv2.line(frame,(x,y),(x2,y2),(0,255,0),1)
+        for n in range(36, 42):
+            x = face_landmarks.part(n).x
+            y = face_landmarks.part(n).y
+            leftEye.append((x, y))
+            next_point = n + 1
+            if n == 41:
+                next_point = 36
+            x2 = face_landmarks.part(next_point).x
+            y2 = face_landmarks.part(next_point).y
+            cv2.line(frame, (x, y), (x2, y2), (0, 255, 0), 1)
 
-        for n in range(42,48):
-        	x = face_landmarks.part(n).x
-        	y = face_landmarks.part(n).y
-        	rightEye.append((x,y))
-        	next_point = n+1
-        	if n == 47:
-        		next_point = 42
-        	x2 = face_landmarks.part(next_point).x
-        	y2 = face_landmarks.part(next_point).y
-        	cv2.line(frame,(x,y),(x2,y2),(0,255,0),1)
+        for n in range(42, 48):
+            x = face_landmarks.part(n).x
+            y = face_landmarks.part(n).y
+            rightEye.append((x, y))
+            next_point = n + 1
+            if n == 47:
+                next_point = 42
+            x2 = face_landmarks.part(next_point).x
+            y2 = face_landmarks.part(next_point).y
+            cv2.line(frame, (x, y), (x2, y2), (0, 255, 0), 1)
 
         left_ear = calculate_EAR(leftEye)
         right_ear = calculate_EAR(rightEye)
 
-        EAR = (left_ear+right_ear)/2
-        EAR = round(EAR,2)
-        if EAR<0.26:
-        	cv2.putText(frame,"DROWSY",(20,100),
-        		cv2.FONT_HERSHEY_SIMPLEX,3,(0,0,255),4)
-        	cv2.putText(frame,"Are you Sleepy?",(20,400),
-        		cv2.FONT_HERSHEY_SIMPLEX,2,(0,0,255),4)
-        	print("Drowsy")
+        EAR = (left_ear + right_ear) / 2
+        EAR = round(EAR, 2)
+        if EAR < 0.2:
+            scan=scan+1
+        else :
+            scan=0
+        if scan>20:
+            threading.Thread(target=play_alarm).start()
+            print("ALARM BERBUNYI")
+            scan=0
         print(EAR)
 
     cv2.imshow("Are you Sleepy", frame)
@@ -64,5 +71,6 @@ while True:
     key = cv2.waitKey(1)
     if key == 27:
         break
+
 cap.release()
 cv2.destroyAllWindows()
